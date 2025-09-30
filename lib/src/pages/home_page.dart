87c9468ee35508e52055800c24c7cfc8d29ac98f
MatoxEdriel <ga99_buggi@hotmail.com>
2025-09-29 22:14:41 -0500

    Concepto de widget

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -10,21 +10,6 @@ class Myhomepage extends StatefulWidget {
 class _MyhomePageState extends State<Myhomepage> {
   int _selectedOption = 0;
 
-  static final List<Widget> _pages = <Widget>[
-    Center(
-      child: Container(
-        margin: const EdgeInsets.all(10.0),
-        color: Colors.amber[600],
-        width: 48.0,
-        height: 48.0,
-      ),
-    ),
-    Center(child: Text("Home", style: TextStyle(fontSize: 30))),
-    Center(child: Text("Configuration", style: TextStyle(fontSize: 30))),
-
-    Center(child: Text("yours pets", style: TextStyle(fontSize: 30))),
-  ];
-
   void _onItemTapped(int currentOption) {
     setState(() {
       _selectedOption = currentOption;
@@ -33,6 +18,20 @@ class _MyhomePageState extends State<Myhomepage> {
 
   @override
   Widget build(BuildContext context) {
+    final List<Widget> pages = <Widget>[
+      Center(
+        child: Container(
+          margin: const EdgeInsets.all(10.0),
+          color: Theme.of(context).colorScheme.secondary,
+          width: 48.0,
+          height: 48.0,
+        ),
+      ),
+      Center(child: Text("Home", style: TextStyle(fontSize: 30))),
+      Center(child: Text("Configuration", style: TextStyle(fontSize: 30))),
+      Center(child: Text("yours pets", style: TextStyle(fontSize: 30))),
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: Row(
@@ -40,25 +39,28 @@ class _MyhomePageState extends State<Myhomepage> {
           children: [
             Image.asset('assets/images/minePet.png', height: 30),
             const SizedBox(width: 8),
+            Text(
+              "MinePet",
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.primary,
+                fontWeight: FontWeight.bold,
+                fontSize: 22,
+              ),
+            )
 
-            const Text("MinePet"),
           ],
         ),
         centerTitle: true,
-        
       ),
-      body: _pages[_selectedOption],
+      body: pages[_selectedOption],
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Start'),
           BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Configuration',
-          ),
+          BottomNavigationBarItem(icon: Icon(Icons.pets), label: 'Your Pets'),
         ],
+
         currentIndex: _selectedOption,
-        selectedItemColor: Colors.amber,
         onTap: _onItemTapped,
       ),
     );
